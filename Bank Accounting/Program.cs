1251e7a774f22dacd0842c30f52a5c86bc1dfcfa
HepiiXDD <mrhappyy14@gmail.com>
2022-04-27 12:59:42 +0200

    withdraw and deposit money

--- Bank Accounting/Program.cs
+++ Bank Accounting/Program.cs
@@ -6,11 +6,24 @@ namespace Bank_Accounting
     {
         static void Main(string[] args)
         {
-            DoLogin();
+            string loggedIn = DoLogin();
+            if (loggedIn == "0")
+                Environment.Exit(0);
+
+            Console.WriteLine("Stan konta: " + GetMoneyDeposited(loggedIn) + " PLN");
+            Console.WriteLine("1. Wypłać pieniądze");
+            Console.WriteLine("2. Wpłać pieniądze");
+
+            string choice = Console.ReadLine();
+            if (choice == "1")
+                WithdrawMoney(loggedIn);
+            else if (choice == "2")
+                DepositMoney(loggedIn);
+
             Console.ReadLine();
         }
 
-        static void DoLogin()
+        static string DoLogin()
         {
             Console.WriteLine("Podaj numer karty: ");
             string cardNumber = Console.ReadLine();
@@ -18,7 +31,22 @@ namespace Bank_Accounting
             string cvcNumber = Console.ReadLine();
             Console.WriteLine("Podaj kod pin: ");
             string pinNumber = Console.ReadLine();
-            Console.WriteLine(Verification(cardNumber, cvcNumber, pinNumber));
+
+            bool verified = Verification(cardNumber, cvcNumber, pinNumber);
+
+            if (!verified)
+            {
+                Console.WriteLine("Złe dane!");
+                return "0";
+            }
+
+
+            else
+            {
+                Console.WriteLine("Logowanie pomyślne!");
+                return "unique-id";
+            }
+
         }
 
         static bool Verification (string cardNumber, string cvcNumber, string pinNumber)
@@ -37,6 +65,39 @@ namespace Bank_Accounting
             }
             else
                 return false;
+        }
+
+        static float GetMoneyDeposited (string uniqueId)
+        {
+            if (uniqueId == "unique-id")
+                return (float)12.33;
+            else
+                return 0;
+        }
+
+        static void WithdrawMoney(string uniqueId)
+        {
+            float accountMoney = GetMoneyDeposited(uniqueId);
+
+            Console.WriteLine("Ile pieniędzy chcesz wypłacić?");
+            float moneyToWithdraw = float.Parse(Console.ReadLine());
+
+            float moneyLeft = accountMoney - moneyToWithdraw;
+
+            if (moneyLeft <= 0)
+                Console.WriteLine("Wprowadzona kwota jest za wysoka!");
+            else
+                Console.WriteLine("Pomyślnie wypłacono " + moneyToWithdraw + " PLN. Pozostała kwota " + moneyLeft + " PLN.");
+        }
+
+        static void DepositMoney(string uniqueId)
+        {
+            float accountMoney = GetMoneyDeposited(uniqueId);
+            Console.WriteLine("Ile chcesz wpłacić?");
+            float moneyToDeposit = float.Parse(Console.ReadLine());
+            accountMoney += moneyToDeposit;
+
+            Console.WriteLine("Wpłacono " + moneyToDeposit + " PLN. Aktualny stan konta to " + accountMoney + " PLN.");
         }
     }
 }
