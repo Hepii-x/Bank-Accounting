993d6544a974089b6a8db76c670104eb59bc37b7
HepiiXDD <mrhappyy14@gmail.com>
2022-04-27 12:35:41 +0200

    Add project files.

---
+++ Bank Accounting/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Bank_Accounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoLogin();
+            Console.ReadLine();
+        }
+
+        static void DoLogin()
+        {
+            Console.WriteLine("Podaj numer karty: ");
+            string cardNumber = Console.ReadLine();
+            Console.WriteLine("Podaj kod cvc: ");
+            string cvcNumber = Console.ReadLine();
+            Console.WriteLine("Podaj kod pin: ");
+            string pinNumber = Console.ReadLine();
+            Console.WriteLine(Verification(cardNumber, cvcNumber, pinNumber));
+        }
+
+        static bool Verification (string cardNumber, string cvcNumber, string pinNumber)
+        {
+            if (cardNumber == "1111222233334444")
+            {
+                if (cvcNumber == "123")
+                {
+                    if (pinNumber == "1234")
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+}
