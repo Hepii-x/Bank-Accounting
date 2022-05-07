41aa24aebb4e2adbf49118c0e4a747b09343b65e
Hepii-x <mrhappyy14@gmail.com>
2022-05-07 20:43:54 +0200

    login and register done

--- Bank Accounting/Program.cs
+++ Bank Accounting/Program.cs
@@ -8,16 +8,46 @@ namespace Bank_Accounting
 
         static void Main(string[] args)
         {
-            Console.WriteLine("KEKW");
-            Console.WriteLine("Gigachad");
             DBFunctions db = new DBFunctions();
+            Console.WriteLine("1. Zaloguj");
+            Console.WriteLine("2. Zarejestruj");
+            string choice = Console.ReadLine();
+            Console.Clear();
+            if (choice == "1")
+            {
+                Console.WriteLine("Podaj numer karty: ");
+                string cardNumber = Console.ReadLine();
+                Console.WriteLine("Podaj kod cvc karty: ");
+                string cvcNumber = Console.ReadLine();
+                Console.WriteLine("Podaj kod pin karty: ");
+                string pinNumber = Console.ReadLine();
 
-            //db.InsertDB();
-            db.GetFromDB();
+                db.Login(cardNumber, cvcNumber, pinNumber);
 
-            Console.ReadLine();
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Podaj imię: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Podaj nazwisko: ");
+                string surname = Console.ReadLine();
+                Console.WriteLine("Ilość pieniędzy do wpłaty: ");
+                float money =  float.Parse(Console.ReadLine());
+                db.Register(name, surname, money);
+                Console.WriteLine("Zarejestrowano! Zaloguj ponownie.");
+            }
+            else
+            {
+                var rand = new Random();
+                string cardNumber1 = rand.Next(00000000, 99999999).ToString();
+                string cardNumber2 =  rand.Next(00000000, 99999999).ToString();
+
+                string cardNumber = cardNumber1 + cardNumber2;
+                Console.WriteLine(cardNumber);
+
+            }
         }
 
-        
+
     }
 }
--- Bank Accounting/TestingFunc.cs
+++ Bank Accounting/TestingFunc.cs
@@ -8,22 +8,47 @@ namespace Bank_Accounting
     {
         Database db = new Database();
 
-        public void InsertDB()
+        public void Register(string name, string surname, float money)
         {
-            Console.WriteLine("Working DB");
-            string query = "INSERT INTO users (name, surname, card_number, card_cvc, card_pin, money) VALUES ('Maciej', 'Szustakowski', '1111222233334444', '123', '1234', '543098123.09')";
-            SQLiteCommand cmd = new SQLiteCommand(query, db.myConnection);
-            cmd.ExecuteNonQuery();
-            Console.WriteLine("Executed");
+            Start:
+            var rand = new Random();
+            string cardNumber1 = rand.Next(10000000, 99999999).ToString();
+            string cardNumber2 = rand.Next(10000000, 99999999).ToString();
+
+            string cardNumber = cardNumber1 + cardNumber2;
+
+            string cvcNumber = rand.Next(100, 999).ToString();
+            string pinNumber = rand.Next(1000, 9999).ToString();
+
+            try
+            {
+                string query = "INSERT INTO users (name, surname, card_number, card_cvc, card_pin, money) VALUES ('" + name + "', '" + surname + "', '"+ cardNumber +"', '" + cvcNumber + "', '" + pinNumber + "', '" + money + "')";
+                SQLiteCommand cmd = new SQLiteCommand(query, db.myConnection);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                goto Start;
+            }
+            
         }
 
-        public void GetFromDB()
+        public void Login(string cardNumber, string cvcNumber, string pinNumber)
         {
-            string query = "SELECT name, surname, card_number FROM users WHERE card_number = '1111222233334444'";
-            SQLiteCommand cmd = new SQLiteCommand(query, db.myConnection);
-            SQLiteDataReader dataX = cmd.ExecuteReader();
-            dataX.Read();
-            Console.WriteLine(dataX["name"] + " " + dataX["surname"] + " " + dataX["card_number"]);
+            try
+            {
+                string query = "SELECT name, surname, money FROM users WHERE card_number = '" + cardNumber + "' AND card_cvc = '" + cvcNumber + "' AND card_pin = '" + pinNumber + "'";
+                SQLiteCommand cmd = new SQLiteCommand(query, db.myConnection);
+                SQLiteDataReader dataX = cmd.ExecuteReader();
+                dataX.Read();
+                Console.WriteLine(dataX["name"] + " " + dataX["surname"] + " " + dataX["money"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Złe dane, zacznij od nowa");
+                Console.ReadLine();
+            }
+            
 
         }
     }
